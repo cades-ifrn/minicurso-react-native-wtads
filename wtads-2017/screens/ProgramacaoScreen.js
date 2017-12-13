33359d5322a99fbb47f17a27832da3013ef5dd35
Francisco Bento <francisco.bento.jr@hotmail.com>
2017-12-13 13:32:34 -0300

    Add step-4

--- wtads-2017/screens/ProgramacaoScreen.js
+++ wtads-2017/screens/ProgramacaoScreen.js
@@ -1,11 +1,60 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, SectionList } from 'react-native';
+
+const Header = ({title}) => {
+  return (
+    <View>
+      <Text>{title}</Text>
+    </View>
+  )
+}
+
+const Item = ({title}) => {
+  return (
+    <View>
+      <Text>{title}</Text>
+    </View>
+  )
+}
 
 export default class ProgramacaoScreen extends React.Component {
+  state = {
+    programacao: []
+  }
+
+  componentDidMount() {
+    fetch('https://rawgit.com/chicobentojr/2710b41a9a376bbcd43a634bb7935fd4/raw/4ca17d5a93f47ee35aa02ec35ea0964a8ae2f6bc/wtads2017.json')
+      .then(response => response.json())
+      .then(({schedule}) => {
+        let key = 1;
+        this.setState({
+          programacao: schedule.map(s => {
+            return {
+              title: s.title,
+              chair: s.chair,
+              data: s.activities.map(a => ({...a, key: key++}))
+            }
+          })
+        });
+      });
+  }
+
+  _renderItem({item}) {
+    return <Item {...item}/>;
+  }
+
+  _renderSectionHeader({section}) {
+    return <Header title={section.title} chair={section.chair}/>;
+  }
+
   render() {
     return (
       <View>
-        <Text>Aqui vem a programaçao!</Text>
+        <SectionList 
+          renderItem={this._renderItem}
+          renderSectionHeader={this._renderSectionHeader}
+          sections={this.state.programacao}
+        />
       </View>
     );
   }
